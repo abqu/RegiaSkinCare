981dbc33470b330dfcb439b90ae395dbc9ab4210
Jose Flores <josecruzflores.fl@gmail.com>
2020-09-27 21:56:14 -0500

    Correctly configure router

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import HelloWorld from '../components/HelloWorld'
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
-    routes: [{}]
+    routes: [{
+	path: '/',
+	name: 'Home',
+	component: HelloWorld
+    }]
 });
 
 export default router;
