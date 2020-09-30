508e4aeb3d893173d35de0664ac9f46051a0d25c
Jose Flores <josecruzflores.fl@gmail.com>
2020-09-30 18:02:04 -0500

    Add new components

---
+++ src/components/Register.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="fill-height">
+    <v-container class="fill-height">
+      <v-row align="center" justify="center">
+        <v-col cols="10" md="10">
+          <v-card class="d-flex align-center" color="white">
+            <v-row class="mx-10 my-10">
+              <v-col cols="12">
+                <h3 class="my-5">Registro</h3>
+                <v-text-field v-model="registerData.email" label="Correo electrónico" single-line solo />
+                <v-text-field v-model="registerData.password" type="password" label="Contraseña" single-line solo/>
+                <v-text-field v-model="registerData.passwordVerify" type="password" label="Confirme su contraseña" single-line solo/>
+                <v-btn medium block color="primary" @click="signUp(registerData.email.toLowerCase(), registerData.password, registerData.passwordVerify)">
+                  Registrarse
+                </v-btn>
+                <h4 align="center" class="mb-2">
+                  <a class="caption" href="/login">
+                    ¿Ya estás registrado?
+                  </a>
+                </h4>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+const {baseURL} = require('../urlHelper');
+
+export default {
+    data() {
+        return {
+            registerData: {email: "", password: "", passwordVerify: ""},
+        }
+    },
+    methods: {
+        signUp(email, password, passwordVerify) {
+            email = email.toLowerCase();
+            if (password !== passwordVerify) {
+                this.$swal({
+                    title: "Contraseñas no coinciden",
+                    icon: "error",
+                    text: "Verifique que ambas contraseñas coincidan",
+                });
+            } else {
+                let data = {email, password};
+                const url = baseURL + '/api/register';
+                axios
+                    .post(url, data)
+                    .then(response => {
+                        localStorage.token = response.data.token;
+                        window.open('/', "_self");
+                    })
+                    .catch(error => {
+                        this.$swal({
+                            title: "Error durante el registro",
+                            icon: "error",
+                            text: error.response.statusText,
+                        });
+                    });
+            }
+        }
+    }
+}
+</script>
