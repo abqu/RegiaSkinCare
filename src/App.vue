ca3966be59fd1363142d96cb65f80633d609c8b3
Jose Flores <josecruzflores.fl@gmail.com>
2020-09-30 18:02:55 -0500

    Finish setting up app

--- src/App.vue
+++ src/App.vue
@@ -5,35 +5,19 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-btn 
+        href="/"
+        text>
+        <h1 class="mr-2">Regia Skin Care</h1>
+      </v-btn>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
+      <v-btn v-if='isNotAuthenticated()'
+        href="/login"
         text
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        <span class="mr-2">Iniciar sesión</span>
       </v-btn>
     </v-app-bar>
 
@@ -52,5 +36,10 @@ export default {
   data: () => ({
     //
   }),
+  methods: {
+    isNotAuthenticated() {
+      return localStorage.getItem("token") === null;
+    }
+  }
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HelloWorld from '../components/HelloWorld'
+import Login from '../components/Login'
+import Home from '../components/Home'
+import Register from '../components/Register'
 
 Vue.use(VueRouter);
 
@@ -8,10 +10,21 @@ const router = new VueRouter({
     mode: 'history',
     base: process.env.BASE_URL,
     routes: [{
-	path: '/',
-	name: 'Home',
-	component: HelloWorld
-    }]
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: Register
+    }
+]
 });
 
 export default router;
