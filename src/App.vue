a6d9c06e37e36429ec7cb01bb67d7ceb263be6ed
Jose Flores <josecruzflores.fl@gmail.com>
2020-10-03 12:57:44 -0500

    Show add button if admin check is passed

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,21 @@
       <v-btn 
         href="/"
         text>
-        <h1 class="mr-2">Regia Skin Care</h1>
+        <v-toolbar-title class="mr-2 pacifico">Regia Skin Care</v-toolbar-title>
       </v-btn>
 
       <v-spacer></v-spacer>
 
+      <v-btn v-if="iAmAdmin()"
+        href="/addItem"
+        text>
+        <v-icon medium>mdi-plus</v-icon>
+      </v-btn>
       <v-btn v-if='isNotAuthenticated()'
         href="/login"
         text
       >
-        <span class="mr-2">Iniciar sesión</span>
+        <v-icon medium>mdi-login</v-icon>
       </v-btn>
     </v-app-bar>
 
@@ -28,6 +33,9 @@
 </template>
 
 <script>
+import axios from "axios";
+const {baseURL} = require('./urlHelper');
+
 export default {
   name: 'App',
 
@@ -39,7 +47,30 @@ export default {
   methods: {
     isNotAuthenticated() {
       return localStorage.getItem("token") === null;
+    },
+    iAmAdmin() {
+      if (localStorage.getItem("token") === null) {
+        return false;
+      }
+      const url = baseURL + '/api/user/amIAdmin';
+      return axios.get(url, {
+        headers: {
+          'sessiontoken' : localStorage.getItem("token"),
+        },
+      }).then(response => response.status === 200)
+      .catch(error => {
+        console.log(error);
+        return false;
+      })
     }
   }
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+
+.pacifico {
+  font-family: 'Pacifico', cursive;
+}
+</style>
