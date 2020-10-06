07a7ae2c9df55508daf2525973d2802dced74e5a
abqu <erick_abqu@hotmail.com>
2020-10-06 02:44:27 -0500

    VerItems
    Home e Items vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,24 @@
 <template>
-    <div class="fill-height">
-        <h2>Home pendiente :)</h2>
+    <div>
+        <app-items v-bind:items="items"></app-items>
     </div>
 </template>
+
+<script>
+import items from './Items.vue';
+
+export default{
+  components:{
+    'app-items':items
+  },
+  data(){
+    return{
+    items: [
+      {name: 'Crema', url: 'https://www.cetaphil.com.mx/sites/g/files/jcdfhc441/files/styles/cp_product_medium/public/Crema%20Humectante_453.png?itok=CDH3rXDH'},
+      {name: 'Polvo', url: 'https://cdn.shopify.com/s/files/1/1365/5917/products/BIS-MC010_7e2d4acc-082a-463d-aef8-48b7479929a3_1024x1024.jpg?v=1588639528'}
+    ]
+
+    }
+  }
+}
+</script>
---
+++ src/components/Items.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="items">
+    <ul v-bind:key="items">
+      <li v-for="item in items" :key="item">
+        <v-img max-height="150" max-width="250" src="item.url"></v-img>
+        <h3>{{item.name}}</h3>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default{
+  props:
+    ['items'],
+    data(){
+      return {
+
+        }
+      }
+
+  }
+  </script>
+
+<style scoped>
+#items{
+  width: 100%;
+  max-width: 1200px;
+  margin: 40px auto;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+ul{
+  display: flex;
+  flex-wrap:wrap;
+  list-style-type: none;
+  padding: 0;
+}
+li{
+  flex-grow: 1;
+  flex-basis: 300px;
+  text-align: center;
+  padding: 30px;
+  border: 1px solid #222;
+}
+
+</style>
