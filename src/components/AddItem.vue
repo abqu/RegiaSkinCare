9f5595419c363bc0b9298bce4eeed41b1b436b62
Jose Flores <josecruzflores.fl@gmail.com>
2020-10-14 16:43:13 -0500

    Make item edition available

--- src/components/AddItem.vue
+++ src/components/AddItem.vue
@@ -7,11 +7,49 @@
             <v-row class="mx-10 my-10">
               <v-col cols="12">
                 <h3 class="my-5">Agregar nuevo producto</h3>
-                <v-text-field v-model="item.name" :rules="[rules.required]" label="Nombre del producto" single-line solo />
-                <v-currency-field v-model="item.price" :rules="[rules.required]" label="Precio del producto" single-line solo/>
-                <v-file-input v-model="item.image" chips accept="image/*" prepend-icon="mdi-camera" :rules="[rules.required]" label="Imagen del producto" single-line solo />
-                <v-textarea v-model="item.description" clearable clear-icon="mdi-close-circle" label="Descripción del producto" />
-                <v-btn medium block color="primary" @click="uploadItem(item.name, item.description, item.price, item.image)">
+                <v-text-field
+                  v-model="item.name"
+                  :rules="[rules.required]"
+                  label="Nombre del producto"
+                  single-line
+                  solo
+                />
+                <v-currency-field
+                  v-model="item.price"
+                  :rules="[rules.required]"
+                  label="Precio del producto"
+                  single-line
+                  solo
+                />
+                <v-file-input
+                  v-model="item.image"
+                  chips
+                  accept="image/*"
+                  prepend-icon="mdi-camera"
+                  :rules="[rules.required]"
+                  label="Imagen del producto"
+                  single-line
+                  solo
+                />
+                <v-textarea
+                  v-model="item.description"
+                  clearable
+                  clear-icon="mdi-close-circle"
+                  label="Descripción del producto"
+                />
+                <v-btn
+                  medium
+                  block
+                  color="primary"
+                  @click="
+                    uploadItem(
+                      item.name,
+                      item.description,
+                      item.price,
+                      item.image
+                    )
+                  "
+                >
                   Agregar nuevo producto
                 </v-btn>
               </v-col>
@@ -24,52 +62,68 @@
 </template>
 
 <script>
-
 import axios from "axios";
-const {baseURL} = require('../urlHelper');
+const { baseURL } = require("../urlHelper");
 
 export default {
-    data() {
-        return {
-            item: {
-                name: "", 
-                description: "", 
-                price: 0,
-            },
-            rules: {
-                required: value => !!value || "Campo obligatorio",
-            },
-        };
-    },
-    methods: {
-      uploadItem(name, description, price, image) {
-        const url = baseURL + '/api/item';
-        const data = new FormData();
-        data.append('image', image);
-        data.append('name', name);
-        data.append('description', description);
-        data.append('price', price);
-        axios
-          .post(url, data, {
-            headers: {
-              sessiontoken : sessionStorage.getItem('token'),
-            },
-          }).then(response => {
-            console.log(response);
-            this.$swal({
-              title: "Se agregó nuevo producto",
-              icon: "success",
-              text: "Se logró agregar el nuevo producto de manera exitosa",
-            });
-            this.$router.push('/');
-          }).catch(error => {
-            this.$swal({
-              title: "Error al agregar artículo",
-              icon: "error",
-              text: error.response.statusText,
-            });
-          });
+  data() {
+    return {
+      item: {
+        name: "",
+        description: "",
+        price: 0,
       },
+      rules: {
+        required: (value) => !!value || "Campo obligatorio",
+      },
+    };
+  },
+  methods: {
+    async uploadItem(name, description, price, image) {
+      const url = baseURL + "/api/item/";
+      const data = new FormData();
+      data.append("image", image);
+      data.append("name", name);
+      data.append("description", description);
+      data.append("price", price);
+      const itemId = this.$route.params.itemId;
+      const headers = {
+        headers: {
+          sessiontoken: sessionStorage.getItem("token"),
+        },
+      };
+      const response = await (itemId
+        ? axios.put(url + itemId, data, headers)
+        : axios.post(url, data, headers));
+      console.log(response);
+      if (!response || !(response.status !== 200 || response.status !== 201)) {
+        this.$swal({
+          title: "Error en la operación",
+          icon: "error",
+          text: "No se logró realizar la operación en el artículo",
+        });
+      } else {
+        this.$swal({
+          title: "Operación exitosa",
+          icon: "success",
+          text: "Se logró realizar la operación en el artículo",
+        });
+        this.$router.push("/");
+      }
+    },
+  },
+  async beforeMount() {
+    if (!this.$route.params.itemId) {
+      return;
     }
-}
+    const URL = baseURL + "/api/item/" + this.$route.params.itemId;
+    const response = await axios.get(URL);
+    if (!response) {
+      return;
+    }
+    this.item.name = response.data.name;
+    this.item.price = (Number(response.data.price) / 100).toFixed(2);
+    this.item.description = response.data.description;
+  },
+};
 </script>
